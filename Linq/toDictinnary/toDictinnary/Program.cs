0c913c55948e1457c2d82e36790525a99e623b98
manduz <emekmanduz3623@gmail.com>
2024-10-23 23:26:48 +0300

    toList,toArray, toDictinnary

---
+++ Linq/toDictinnary/toDictinnary/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace toDictinnary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //anahtarlar benzersiz olmalıdır.
+            //ToDictionary() bir koleksiyondan bir sözlük (Dictionary) oluşturmanızı sağlar.
+            //Bu metod, sorgu sonucunda her öğeyi bir anahtar ve bir değer ile ilişkilendirir.
+            //bir koleksiyon kişilerin adlarını anahtar, yaşlarını değer olarak kullanarak bir sözlük oluşturuyor.
+            List<Person> people = new List<Person>
+            {
+                new Person { Name = "Alice", Age = 25 },
+                new Person { Name = "Bob", Age = 30 },
+                new Person { Name = "Charlie", Age = 35 }
+            };
+
+            // Kişileri isimlerini anahtar, yaşlarını değer olarak kullanarak bir sözlük oluşturma
+            Dictionary<string, int> peopleDictionary = people.ToDictionary(p => p.Name, p => p.Age);
+
+            foreach (var kvp in peopleDictionary)
+            {
+                Console.WriteLine($"Name: {kvp.Key}, Age: {kvp.Value}");
+            }
+            Console.ReadLine();
+        }
+
+    }
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
---
+++ Linq/toList_toArray/toList_toArray/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace toList_toArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //ToList() ve ToArray() metotları, sorgu sonucunda elde edilen verileri liste ya da dizi (array) tipine dönüştürmek için kullanılır.
+
+            int[] numbers = { 1, 2, 3, 4, 5 };
+
+            
+
+
+            // LINQ sorgusu: Çift sayıları bul ve bir listeye dönüştür
+            List<int> evenNumbers = numbers.Where(n => n % 2 == 0).ToList();
+
+            foreach (var number in evenNumbers)
+            {
+                Console.WriteLine(number); // Çıktı: 2, 4
+            }
+
+            ///*************************//
+            ///
+            int[] numbers2 = { 10, 20, 30, 40, 50 };
+
+            //List<int> numbers = new List<int>() { 1, 2, 3, 4, 5 };
+            //int [ numberList = numbers.toArray();
+
+
+            // LINQ sorgusu: Çift sayıları bul ve bir diziye dönüştür
+            int[] evenNumbersArray = numbers2.Where(n => n % 2 == 0).ToArray();
+
+            foreach (var number in evenNumbersArray)
+            {
+                Console.WriteLine(number); 
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
